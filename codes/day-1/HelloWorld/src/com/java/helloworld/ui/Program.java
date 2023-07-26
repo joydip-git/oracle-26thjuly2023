563ef84d85e121a3d0e91f77153e58426ad39a3f
joydip mondal <joydip.mondal@gmail.com>
2023-07-26 12:36:53 +0530

    hello world created

---
+++ codes/day-1/HelloWorld/src/com/java/helloworld/ui/Program.java
@@ -0,0 +1,24 @@
+package com.java.helloworld.ui;
+
+//import java.io.PrintStream;
+
+public class Program {
+	public static void main(String[] args) {
+		/*PrintStream ps = System.out;
+		ps.println("Hello world");*/
+		System.out.println("Main method");
+		Program.print();
+		
+		//display();
+		Program p = new Program();
+		p.display();		
+	}
+	static void print() {
+		System.out.println("Hello world");
+	}
+	
+	//non-static or instance method
+	void display() {
+		System.out.println("display something");
+	}
+}
