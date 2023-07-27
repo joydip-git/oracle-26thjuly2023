642a246ae28898b285b69ef93fe088b702e6a5d8
joydip mondal <joydip.mondal@gmail.com>
2023-07-27 11:57:45 +0530

    class update

--- codes/day-2/ClassinJava/src/com/java/classinjava/entities/Car.java
+++ codes/day-2/ClassinJava/src/com/java/classinjava/entities/Car.java
@@ -2,36 +2,101 @@ package com.java.classinjava.entities;
 
 public class Car {
 	// private attributes
-	String chasisNumber;
+	private String chasisNumber;
 	private String bodyColor;
-	private int noOfGears;
-	private String modeOfTransmission;
-	private int seatingCapacity;
-	private int makeYear;
 	private String companyName;
 	private String modelName;
+	private String modeOfTransmission;
+
+	private int noOfGears;
+	private int seatingCapacity;
+	private int makeYear;
 
 	// constructor: default
 	public Car() {
-		// bodyColor = null;
-		// noOfGears=0;
 	}
 
-	// constructor: parameterized
-	public Car(String chasis, String color, String model, String company, String transmission, int capacity, int gears,
-			int year) {
-
-		bodyColor = color;
-		chasisNumber = chasis;
-		modelName = model;
-		companyName = company;
-		modeOfTransmission = transmission;
-		seatingCapacity = capacity;
-		noOfGears = gears;
-		makeYear = year;
+	// constructor: parameterized constructor
+	public Car(String chasisNumber, String bodyColor, String companyName, String modelName, String modeOfTransmission,
+			int noOfGears, int seatingCapacity, int makeYear) {
+		super();
+		this.chasisNumber = chasisNumber;
+		this.bodyColor = bodyColor;
+		this.companyName = companyName;
+		this.modelName = modelName;
+		this.modeOfTransmission = modeOfTransmission;
+		this.noOfGears = noOfGears;
+		this.seatingCapacity = seatingCapacity;
+		this.makeYear = makeYear;
 	}
 
-	// public methods
+	//getters and setters
+	
+	public String getChasisNumber() {
+		return chasisNumber;
+	}
+
+	public void setChasisNumber(String chasisNumber) {
+		this.chasisNumber = chasisNumber;
+	}
+
+	public String getBodyColor() {
+		return bodyColor;
+	}
+
+	public void setBodyColor(String bodyColor) {
+		this.bodyColor = bodyColor;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getModelName() {
+		return modelName;
+	}
+
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+
+	public String getModeOfTransmission() {
+		return modeOfTransmission;
+	}
+
+	public void setModeOfTransmission(String modeOfTransmission) {
+		this.modeOfTransmission = modeOfTransmission;
+	}
+
+	public int getNoOfGears() {
+		return noOfGears;
+	}
+
+	public void setNoOfGears(int noOfGears) {
+		this.noOfGears = noOfGears;
+	}
+
+	public int getSeatingCapacity() {
+		return seatingCapacity;
+	}
+
+	public void setSeatingCapacity(int seatingCapacity) {
+		this.seatingCapacity = seatingCapacity;
+	}
+
+	public int getMakeYear() {
+		return makeYear;
+	}
+
+	public void setMakeYear(int makeYear) {
+		this.makeYear = makeYear;
+	}
+
+	// methods
 	public String getInfo() {
 		return modelName + ", " + bodyColor + ", " + seatingCapacity + ", " + noOfGears;
 	}
--- codes/day-2/ClassinJava/src/com/java/classinjava/ui/Program.java
+++ codes/day-2/ClassinJava/src/com/java/classinjava/ui/Program.java
@@ -9,9 +9,14 @@ public class Program {
 		System.out.println(a);
 		// create is a reference variable (present in stack), which stores reference of
 		// an Car instance (present in the heap)
-		//Car creta = null;
-		//creta = new Car("axhp132344", "grey", "Creta", "Hyundai", "automatic", 5, 6, 2022);
-		Car creta = new Car("axhp132344", "grey", "Creta", "Hyundai", "automatic", 5, 6, 2022);
+		// Car creta = null;
+		// creta = new Car(null, "grey", "Creta", "Hyundai", "automatic", 5, 6, 2022);
+		Car creta = new Car("axhp124445", "grey", "Creta", "Hyundai", "automatic", 5, 6, 2022);
+		/*Car creta = new Car();
+		creta.setChasisNumber(chasisNumber);*/
+		//creta.setChasisNumber("axhp124445");
+		
+		System.out.println(creta.getChasisNumber());
 		System.out.println(creta.getInfo());
 	}
 
